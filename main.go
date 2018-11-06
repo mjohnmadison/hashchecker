c89462aeeb60f80ce7a7b9bc8a860a903c2fb3f6
Matt John <mjohn@zendesk.com>
2018-11-05 20:02:37 -0600

    Update loop logic and error handling

--- main.go
+++ main.go
@@ -1,9 +1,11 @@
 package main
 
+// Version: 1.3
 // Script to check hashrate of miner and reset if it falls below expected hashrate
 // Script will kill and restart the miner a few times and recheck hashrate
 // If hashrate continues to be low, it will restart the computer
 // Requries 2 arguments. First is wallet address/api key, second is expected hashrate
+// TODO: Log errors or warnings to logfile
 
 import (
 	"encoding/json"
@@ -20,7 +22,7 @@ import (
 	"github.com/fatih/color"
 )
 
-type hash struct {
+type stats struct {
 	Status string `json:"status"`
 	Data   struct {
 		Time             int         `json:"time"`
@@ -42,12 +44,19 @@ type hash struct {
 
 func main() {
 	// Variables
-	retry := 1       // Set initial retry
+	retry := 0       // Set initial retry
 	wt := os.Args[1] // Your wallet address.
 	er := os.Args[2] // Expected reported hashrate of the miner.
 	for {            // Start the loop
+		if !minerRunning() { // Check miner is running each time
+			startMiner()
+			retry++
+		}
 		url := "https://api.ethermine.org/miner/" + wt + "/currentStats"
-		cur := currentHashRate(url)
+		cur, err := currentHashRate(url)
+		if err != nil {
+			log.Println(err)
+		}
 		hr, _ := strconv.Atoi(er) // Convert Expected hashrate into int
 		// Compare actual hashrate from the pool and what your miner should be getting
 		if !checkHashRate(cur, hr) {
@@ -56,82 +65,79 @@ func main() {
 		} else {
 			retry = 1 // Reset retry counter if we get success
 		}
-		time.Sleep(30 * time.Second)
+		time.Sleep(60 * time.Second) // Check stats every 60 seconds
 	}
 }
 
-func currentHashRate(url string) int {
+func currentHashRate(url string) (int, error) {
 	webClient := http.Client{
-		Timeout: time.Second * 2, // Maximum of 2 secs
+		Timeout: time.Second * 5, // Maximum of 2 secs
 	}
 
 	req, err := http.NewRequest(http.MethodGet, url, nil)
 	if err != nil {
-		log.Println(err)
+		return 0, err
 	}
 
 	req.Header.Set("User-Agent", "Mozilla")
 
 	res, getErr := webClient.Do(req)
 	if getErr != nil {
-		log.Println(getErr)
+		return 0, getErr
 	}
 
 	body, readErr := ioutil.ReadAll(res.Body)
 	if readErr != nil {
-		log.Println(readErr)
+		return 0, readErr
 	}
 
-	hashrate := hash{}
+	hashrate := stats{}
 	jsonErr := json.Unmarshal(body, &hashrate)
 	if jsonErr != nil {
-		log.Println(jsonErr)
+		return 0, jsonErr
 	}
 
-	return hashrate.Data.ReportedHashrate / 1000000
+	return hashrate.Data.ReportedHashrate / 1000000, nil
 }
 
 func checkHashRate(rate int, expected int) bool {
 	if rate < expected {
-		color.Red("Hashrate Bad: %d", rate)
+		color.Red("Hashrate Bad: %d\n", rate)
 		return false
 	}
-	color.Green("Hashrate Good: %d", rate)
+	color.Green("Hashrate Good: %d\n", rate)
 	return true
 }
 
 func fixMiner(retry int) {
-	if retry > 3 {
+	if retry > 2 {
 		reboot()
 	}
-	if runtime.GOOS == "windows" {
-		color.Yellow("Fixing miner")
-		if !minerRunningWin() {
-			startMiner()
-			retry++
-		} else {
-			// Restart it and add to retry var
-			color.Yellow("Restarting Miner")
-			killMiner()
-			startMiner()
-			retry++
-		}
-	} else if runtime.GOOS == "linux" {
-		color.Yellow("Fixing miner")
-		if !minerRunningLin() {
-			startMiner()
-			retry++
-		} else {
-			// Restart it and add to retry var
-			color.Yellow("Restarting Miner")
-			killMiner()
-			startMiner()
-			retry++
-		}
+	color.Yellow("Attemping to reset miner...")
+	if minerRunning() {
+		// Restart it and add to retry var
+		killMiner()
+		startMiner()
+		retry++
 	}
 }
 
+func minerRunning() bool {
+	if runtime.GOOS == "windows" {
+		if minerRunningWin() {
+			return true
+		}
+	} else if runtime.GOOS == "linux" {
+		if minerRunningLin() {
+			return true
+		}
+	}
+	return false
+}
+
 func minerRunningWin() bool {
+	c := color.New(color.FgGreen, color.Bold)
+	r := color.New(color.FgRed, color.Bold)
 	// Windows sucks, need to find a better way to run a pipe command or get this working:
 	// tasklist | find /I /C "EthDcrMiner" -- this would spit out number of those prcesses as int
 	// For now, we make it janky
@@ -141,23 +147,27 @@ func minerRunningWin() bool {
 	}
 	pid := strings.TrimSpace(string(p))
 	if strings.Contains(pid, "EthDcrMiner") {
-		color.Green("Miner is running")
+		c.Printf("Miner running: ")
 		return true
 	}
+	r.Printf("Miner NOT running: ")
 	return false
 }
 
 func minerRunningLin() bool {
-	cmd := "ps aux | grep EthDcrMiner | wc -l"
+	c := color.New(color.FgGreen, color.Bold)
+	r := color.New(color.FgRed, color.Bold)
+	cmd := "ps aux | grep [E]thDcrMiner | wc -l" // Regex to eliminate the grep returning the grep command.
 	p, err := exec.Command("bash", "-c", cmd).Output()
 	if err != nil {
 		log.Println("EXEC ERROR:", err)
 	}
 	pid, _ := strconv.Atoi(strings.TrimSpace(string(p)))
 	if pid > 0 {
-		color.Green("Miner is running")
+		c.Printf("Miner running: ")
 		return true
 	}
+	r.Printf("Miner NOT running: ")
 	return false
 }
 
@@ -172,23 +182,24 @@ func killMiner() {
 }
 
 func startMiner() {
-	color.Green("Starting Miner")
+	c := color.New(color.FgCyan, color.Bold)
+	c.Printf("Starting Miner... ")
 	if runtime.GOOS == "windows" {
 		cmd := exec.Command("cmd", "/C", "start", "cmd", "/C", "start-miner.lnk") // FTW Winbloze
 		cmd.Start()                                                               // Fork miner
-	} else {
+	} else { // Linux
 		cmd := exec.Command("bash", "-c", "./start-miner.sh")
 		cmd.Start() // Fork miner
 	}
-	time.Sleep(600 * time.Second) // Wait 10 minutes for status to populate on pool
+	time.Sleep(600 * time.Second) // Wait 10 minutes for stats to populate on pool
 }
 
 func reboot() {
-	color.Red("Rebooting Machine; too many failures in a row")
+	color.Red("Rebooting Machine: too many failures in a row")
 	if runtime.GOOS == "windows" {
-		exec.Command("cmd", "/C", "shutdown /r /t 00").CombinedOutput() // Bye Winders
+		exec.Command("cmd", "/C", "shutdown /r /t 00").CombinedOutput()
 	} else {
 		exec.Command("bash", "-c", "sudo reboot").CombinedOutput()
 	}
-	time.Sleep(5 * time.Second)
+	os.Exit(1)
 }
